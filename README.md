e990ad5b8f74698fdb28917a7ae178bd428dc592
Ivan Moura <ivan.clay.moura@gmail.com>
2023-10-14 11:05:49 +0000

    configure `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Tabnews.com.br clone
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Jovem, você precisa estudar!!!</h1>
+function Home() {
+  return <h1>Jovem, você precisa estudar!!!</h1>;
+}
+
+function teste() {
+  console.log("executa funcao");
 }
 
 export default Home;
